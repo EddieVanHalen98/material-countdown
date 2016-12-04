61a79facb0ced76f59ae658c934a7b6e935d2e9d
James T Saeed <jts@evh98.com>
2016-12-04 21:31:34 +0000

    Updated ActionBar in NewCountdownActivity

--- app/src/main/java/evh98/com/materialcountdown/NewCountdownActivity.java
+++ app/src/main/java/evh98/com/materialcountdown/NewCountdownActivity.java
@@ -2,6 +2,9 @@ package evh98.com.materialcountdown;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class NewCountdownActivity extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class NewCountdownActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_countdown);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_new_countdown, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.menu_close:
+                return true;
+            case R.id.menu_save:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
