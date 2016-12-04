44cbeef12ce4af97416fcd0621d9231458b29594
James T Saeed <jts@evh98.com>
2016-12-04 21:11:46 +0000

    Added navigation drawer

--- app/src/main/java/evh98/com/materialcountdown/NewCountdownFragment.java
+++ app/src/main/java/evh98/com/materialcountdown/NewCountdownFragment.java
@@ -14,7 +14,7 @@ public class NewCountdownFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_clock, container, false);
+        View view = inflater.inflate(R.layout.fragment_new_countdown, container, false);
         return view;
     }
 
