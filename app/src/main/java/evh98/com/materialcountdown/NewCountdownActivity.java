df7a42a92ee864b6cc1c57097eadd50e6f3fd38e
EddieVanHalen98 <jts@evh98.com>
2016-12-06 13:25:57 +0000

    New Countdown UI looks more like Calendar New Event

--- app/src/main/java/evh98/com/materialcountdown/NewCountdownActivity.java
+++ app/src/main/java/evh98/com/materialcountdown/NewCountdownActivity.java
@@ -1,40 +1,49 @@
 package evh98.com.materialcountdown;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class NewCountdownActivity extends AppCompatActivity {
+
+    private EditText tf_name;
+    private Button b_date;
+    private Button b_time;
+    private Button b_color;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_countdown);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        tf_name = (EditText) findViewById(R.id.tf_form_name);
+        b_date = (Button) findViewById(R.id.b_form_date);
+        b_time = (Button) findViewById(R.id.b_form_time);
+        b_color = (Button) findViewById(R.id.b_form_color);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_new_countdown, menu);
-        return true;
+    public void onFormNameClick(View view) {
+        tf_name.setCursorVisible(true);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
+    public void onFormDateClick(View view) {
 
-        switch (id) {
-            case R.id.menu_close:
-                finish();
-                return true;
-            case R.id.menu_save:
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+    }
+
+    public void onFormTimeClick(View view) {
+
     }
 }
